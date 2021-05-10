fc48b42ba08dfad51f0fa0e463d3bb3a41e2f32e
ysw7042 <77568456+ysw7042@users.noreply.github.com>
2021-05-10 23:02:55 +0900

    windows

--- CarApp.java
+++ CarApp.java
@@ -1,46 +1,32 @@
-package oop;
+package inheritance;
 
-//Car 클래스를 이용하여 작성된 프로그램
 public class CarApp {
 	public static void main(String[] args) {
-		//클래스로 객체(인스턴스)를 생성하여 참조변수에 저장
-		//형식) 클래스명 참조변수=new 클래스명()
-		//객체(인스턴스)를 생성하면 인스턴스 필드에는 기본값이 자동으로 저장되어 초기화
-		// => 기본값 - 숫자형 : 0, 논리형 : false, 참조형 : null 
-		Car carOne=new Car();
-		Car carTwo=new Car();
+		Car car=new Car("홍길동", "싼타페");
 		
-		System.out.println("carOne = "+carOne);
-		System.out.println("carTwo = "+carTwo);
-		System.out.println("=====================================================");
-		//참조변수를 이용하여 참조변수에 저장된 객체(인스턴스)의 요소에 접근하여 사용
-		// => . 연산자를 이용하여 객체(인스턴스)의 요소(필드 또는 메소드) 접근
-		carOne.aliasName="티코";
-		carOne.engineStatus=false;
-		carOne.currentSpeed=0;
+		System.out.println("소유주 = "+car.getUserName());
+		System.out.println("모델명 = "+car.getModelName());
+		System.out.println("=========================================================");
+		//Car 클래스는 Object 클래스를 상속 받기 때문에 Object 클래스의 메소드 호출 가능
+		//Object.toString() : 인스턴스를 문자열로 변환하여 반환하는 메소드
+		//Car 클래스에서 toString() 메소드를 오버라이드 선언하면 Object 클래스의 toString()
+		//메소드는 숨겨지고 Car 클래스의 toString() 메소드 호출 - Car 인스턴스의 필드값 확인 
+		System.out.println("car.toString() = "+car.toString());
 		
-		System.out.println("첫번째 자동차의 별칭 = "+carOne.aliasName);
-		System.out.println("첫번째 자동차의 엔진상태 = "+carOne.engineStatus);
-		System.out.println("첫번째 자동차의 현재속도 = "+carOne.currentSpeed);
-		System.out.println("=====================================================");
-		carTwo.aliasName="싼타페";
+		//참조변수에 저장된 인스턴스를 출력할 경우 Object 클래스의 toString() 메소드가
+		//자동 호출하여 인스턴스를 문자열로 반환받아 출력
+		System.out.println("car = "+car);
+		System.out.println("=========================================================");
+		String name="홍길동";
 		
-		System.out.println("두번째 자동차의 별칭 = "+carTwo.aliasName);
-		System.out.println("두번째 자동차의 엔진상태 = "+carTwo.engineStatus);
-		System.out.println("두번째 자동차의 현재속도 = "+carTwo.currentSpeed);
-		System.out.println("=====================================================");
-		//객체(인스턴스)를 이용하여 메소드 호출 - 기능 구현
-		carOne.startEngine();
-		carOne.speedUp(30);
-		carOne.speedUp(50);
-		carOne.speedDown(40);
-		carOne.speedZero();
-		carOne.stopEngine();
-		System.out.println("=====================================================");
+		//String 클래스에서 toString() 메소드를 오버라이드 선언하여 String 클래스의 toString() 메소드 호출
+		//String.toString() : String 인스턴스에 저장된 문자열을 반환하는 메소드
+		System.out.println("name.toString() = "+name.toString());
+		//참조변수의 인스턴스를 출력할 경우 자동으로 toString() 메소드 호출
+		System.out.println("name = "+name);
+		System.out.println("=========================================================");
 	}
 }
-
-
 
 
 
